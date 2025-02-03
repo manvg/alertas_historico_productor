8593d0ea70e7cdfe9aa206c4f940293a24dd2c13
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-03 01:15:54 -0300

    Creación proyecto java spring boot para implementar alertas con rabbitmq

---
+++ src/main/java/com/alertas_historico/alertas_historico_productor/AlertasHistoricoProductorApplication.java
@@ -0,0 +1,13 @@
+package com.alertas_historico.alertas_historico_productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasHistoricoProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertasHistoricoProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alertas_historico/alertas_historico_productor/AlertasHistoricoProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertas_historico.alertas_historico_productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertasHistoricoProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
